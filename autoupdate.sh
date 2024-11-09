1e9b41f07d3fa428912376835b21e1025c627ed2
ecegroup2 <projectimposs@gmail.com>
2024-11-09 18:48:02 +0530

    Added some backup features

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,6 +1,15 @@
 #! /usr/bin/bash
 
-# updates backend whenever availaible
+# updates whenever availaible
 cd /home/sp/code/backend
 git pull
+cd /home/sp/code/scripts
+git pull
 
+#Backup old files
+cd /home/sp/log
+cp pycap.log pycap.log.bak
+cp spboot.log spboot.log.bak
+
+python3 -u /home/sp/code/scripts/starter.py >> /home/sp/log/pycap.log &
+java -jar /home/sp/code/backend/backend-0.0.1-SNAPSHOT.jar >> /home/sp/log/spboot.log &
